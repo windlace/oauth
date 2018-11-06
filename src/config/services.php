b49e5f975d0264bc039eb09d3025e68ebc392cc9
windlace <44790541+windlace@users.noreply.github.com>
2018-11-06 07:35:18 +0100

    Update services.php

--- src/config/services.php
+++ src/config/services.php
@@ -3,9 +3,9 @@
 return [
 
     'bitbucket' => [
-        'client_id' => env('BESTCHAIN_OAUTH_BITBUCKET_CLIENT_ID'),
-        'client_secret' => env('BESTCHAIN_OAUTH_BITBUCKET_SECRET'),
-        'redirect' => env('BESTCHAIN_OAUTH_BITBUCKET_REDIRECT'),
+        'client_id' => env('CAST_OAUTH_BITBUCKET_CLIENT_ID'),
+        'client_secret' => env('CAST_OAUTH_BITBUCKET_SECRET'),
+        'redirect' => env('CAST_OAUTH_BITBUCKET_REDIRECT'),
     ],
 
 ];
